7fcc2b5132c765f8b2f2d8b96b7ba9c9a76893c8
flow1981 <florian.stagliano@gmx.de>
2021-03-03 22:14:10 +0000

    Refactor lambda function

--- terraform/modules/static-website/lambda.tf
+++ terraform/modules/static-website/lambda.tf
@@ -1,15 +1,52 @@
-data "archive_file" "lambda" {
+# Data source containing the lambda function and for putting its zipped version
+data "archive_file" "lambda_zip" {
   type = "zip"
-  source_file = "./lambda/index.js"
-  output_path = "./lambda/lambda.zip"
+  source_file = "../modules/static-website/lambda/index.js"
+  output_path = "../modules/static-website/lambda/index.zip"
 }
 
-resource "aws_lambda_function" "example_test_function" {
-  filename = "${data.archive_file.lambda.output_path}"
-  function_name = "tle-cors"
-  role = "${aws_iam_role.example_api_role.arn}"
+# Lambda Function
+resource "aws_lambda_function" "lambda" {
+  function_name     = "tle-proxy-js"
+
+  filename          = data.archive_file.lambda_zip.output_path
+  source_code_hash  = filebase64sha256(data.archive_file.lambda_zip.output_path)
+  role              = aws_iam_role.lambda_execution_role.arn
+
   handler = "index.handler"
-  runtime = "nodejs4.3"
-  source_code_hash = "${base64sha256(file("${data.archive_file.lambda.output_path}"))}"
+  runtime = "nodejs12.x"
+
   publish = true
+}
+
+# IAM role for Lambda Function
+resource "aws_iam_role" "lambda_execution_role" {
+  description        = "Execution role for Lambda functions"
+  name               = "iam-lambda-role"
+
+  assume_role_policy = <<EOF
+{
+        "Version"  : "2012-10-17",
+        "Statement": [
+            {
+                "Action"   : "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid"   : ""
+            }
+        ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_gateway_invoke_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
 }
