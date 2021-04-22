fc5d1d40ae01fe8e5b7a29c2b80bf4124496631a
flow1981 <florian.stagliano@gmx.de>
2021-04-22 21:32:30 +0100

    Load a lot of models (works on mobile)

--- app/nft.js
+++ app/nft.js
@@ -1,7 +1,14 @@
+import { createEarthGnonomic } from './resources/threeJS/models/earth.js'
+import { createIssPositionMarker, addIssModelToMarker } from './resources/threeJS/models/iss.js'
+import { initOrbitalPosition, updateOrbitalPostion, visualizeOrbit, alignXeciToVernalEquinox, alignISSrelativeEarthSurface} from './resources/helper/sat.js'
+
 const TLE_SOURCE =  'https://tle.ar-iss-tracker.info'
 
 const CAMERA_PARAM_URL = '../resources/data/camera_para.dat'
 const NFT_MARKER_URL = './resources/dataNFT/pinball'
+
+const scaleFactor = 1/100
+const earthRadius = 6371    //km
 
 const ARThreeOnLoad = function(tle) {
   console.log('inside ARThree', tle)
@@ -9,6 +16,9 @@ const ARThreeOnLoad = function(tle) {
   onSuccess: function(arScene, arController, arCamera) {
 
     document.body.className = arController.orientation;
+
+    let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.8 );
+    arScene.scene.add( ambientLight );
 
     let renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.gammaOutput = true;
@@ -32,22 +42,45 @@ const ARThreeOnLoad = function(tle) {
 
     document.body.insertBefore(renderer.domElement, document.body.firstChild);
 
-    let sphere = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5, 8, 8),
-      new THREE.MeshNormalMaterial()
-    );
+    // x positive - left, y positive - up, z positive -towards viewer | x, y zero is bottom right of trigger
+    let modelGroup = new THREE.Group();
+    modelGroup.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1)); // we need flip the objects since ARtoolkit displays them mirrored
+    modelGroup.position.set(80,80,80)
 
-    sphere.material.flatShading;
-    sphere.position.z = 40;
-    sphere.position.x = 80;
-    sphere.position.y = 80;
-    sphere.scale.set(80,80,80);
+    let earth = createEarthGnonomic(earthRadius, scaleFactor)
+    earth = alignXeciToVernalEquinox(earth)
+    modelGroup.add(earth)
+
+    let issPosition = createIssPositionMarker()
+    issPosition.scale.set(200,200,200);
+    issPosition = initOrbitalPosition(issPosition, tle, 0, scaleFactor)
+    issPosition = updateOrbitalPostion(issPosition, scaleFactor)
+    modelGroup.add(issPosition)
+
+    let orbit = visualizeOrbit(issPosition.userData.satrec, scaleFactor)
+    modelGroup.add(orbit)
+
+    // addIssModelToMarker(issPosition, ISS_MODEL_URL)
+    alignISSrelativeEarthSurface(issPosition)
+    
+    modelGroup.rotateOnAxis( new THREE.Vector3(1, 0, 0).normalize(), 90 * Math.PI/180 );
+
+    // let sphere = new THREE.Mesh(
+    //   new THREE.SphereGeometry(0.5, 8, 8),
+    //   new THREE.MeshNormalMaterial()
+    // );
+
+    // sphere.material.flatShading;
+    // sphere.position.z = 40;
+    // sphere.position.x = 80;
+    // sphere.position.y = 80;
+    // sphere.scale.set(80,80,80);
 
     arController.loadNFTMarker(
       NFT_MARKER_URL,
       function(markerId) {
         let markerRoot = arController.createThreeNFTMarker(markerId);
-        markerRoot.add(sphere);
+        markerRoot.add(modelGroup);
         arScene.scene.add(markerRoot);
       }
     );
